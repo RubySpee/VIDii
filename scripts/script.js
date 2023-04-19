4fa0c2b47789e1042c5cfc619192d497b0808528
RubySpee <94399952+RubySpee@users.noreply.github.com>
2023-04-19 10:53:50 +0200

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,2 +1,10 @@
 // JavaScript Document
 console.log("Howdy!");
+
+// Bron: https://www.geeksforgeeks.org/how-to-show-images-on-click-using-html/
+function show () {
+    let image = document.getElementById("pikachu1");
+
+    pikachu1.src = "../images/pikachu.png";
+}
+
