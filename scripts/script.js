617e1eb3615ef1e48f144c997144a5d7b0423d84
RubySpee <94399952+RubySpee@users.noreply.github.com>
2023-04-21 08:49:57 +0200

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,10 +1,446 @@
 // JavaScript Document
 console.log("Howdy!");
 
-// Bron: https://www.geeksforgeeks.org/how-to-show-images-on-click-using-html/
-function show () {
-    let image = document.getElementById("pikachu1");
 
-    pikachu1.src = "../images/pikachu.png";
+////
+//button en image 1
+////
+var button1996 = document.querySelector("ol li:nth-of-type(1) button");
+
+function showPika1() {
+    var currentPika = document.querySelector("main > section:first-of-type img.show");
+    if(currentPika) {
+        currentPika.classList.remove("show")
+    }
+
+    var pika1 = document.querySelector("main > section:first-of-type img:nth-of-type(1)");
+    pika1.classList.add("show");
 }
 
+button1996.addEventListener("click", showPika1);
+
+
+////
+//button en image 2
+////
+var button1998serie = document.querySelector("ol li:nth-of-type(2) button");
+
+function showPika2() {
+    var currentPika = document.querySelector("main > section:first-of-type img.show");
+    if(currentPika) {
+        currentPika.classList.remove("show")
+    }
+
+    var pika2 = document.querySelector("main > section:first-of-type img:nth-of-type(2)");
+    pika2.classList.add("show");
+}
+
+button1998serie.addEventListener("click", showPika2);
+
+
+////
+//button en image 3
+////
+var button1998game = document.querySelector("ol li:nth-of-type(3) button");
+
+function showPika3() {
+    var currentPika = document.querySelector("main > section:first-of-type img.show");
+    if(currentPika) {
+        currentPika.classList.remove("show")
+    }
+
+    var pika3 = document.querySelector("main > section:first-of-type img:nth-of-type(3)");
+    pika3.classList.add("show");
+}
+
+button1998game.addEventListener("click", showPika3);
+
+
+////
+//button en image 4
+////
+var button1999 = document.querySelector("ol li:nth-of-type(4) button");
+
+function showPika4() {
+    var currentPika = document.querySelector("main > section:first-of-type img.show");
+    if(currentPika) {
+        currentPika.classList.remove("show")
+    }
+
+    var pika4 = document.querySelector("main > section:first-of-type img:nth-of-type(4)");
+    pika4.classList.add("show");
+}
+
+button1999.addEventListener("click", showPika4);
+
+
+////
+//button en image 5
+////
+var button2001 = document.querySelector("ol li:nth-of-type(5) button");
+
+function showPika5() {
+    var currentPika = document.querySelector("main > section:first-of-type img.show");
+    if(currentPika) {
+        currentPika.classList.remove("show")
+    }
+
+    var pika5 = document.querySelector("main > section:first-of-type img:nth-of-type(5)");
+    pika5.classList.add("show");
+}
+
+button2001.addEventListener("click", showPika5);
+
+
+////
+//button en image 6
+////
+var button2008 = document.querySelector("ol li:nth-of-type(6) button");
+
+function showPika6() {
+    var currentPika = document.querySelector("main > section:first-of-type img.show");
+    if(currentPika) {
+        currentPika.classList.remove("show")
+    }
+
+    var pika6 = document.querySelector("main > section:first-of-type img:nth-of-type(6)");
+    pika6.classList.add("show");
+}
+
+button2008.addEventListener("click", showPika6);
+
+
+////
+//button en image 7
+////
+var button2010 = document.querySelector("ol li:nth-of-type(7) button");
+
+function showPika7() {
+    var currentPika = document.querySelector("main > section:first-of-type img.show");
+    if(currentPika) {
+        currentPika.classList.remove("show")
+    }
+
+    var pika7 = document.querySelector("main > section:first-of-type img:nth-of-type(7)");
+    pika7.classList.add("show");
+}
+
+button2010.addEventListener("click", showPika7);
+
+
+////
+//button en image 8
+////
+var button2016 = document.querySelector("ol li:nth-of-type(8) button");
+
+function showPika8() {
+    var currentPika = document.querySelector("main > section:first-of-type img.show");
+    if(currentPika) {
+        currentPika.classList.remove("show")
+    }
+
+    var pika8 = document.querySelector("main > section:first-of-type img:nth-of-type(8)");
+    pika8.classList.add("show");
+}
+
+button2016.addEventListener("click", showPika8);
+
+
+////
+//button en image 9
+////
+var button2017 = document.querySelector("ol li:nth-of-type(9) button");
+
+function showPika9() {
+    var currentPika = document.querySelector("main > section:first-of-type img.show");
+    if(currentPika) {
+        currentPika.classList.remove("show")
+    }
+
+    var pika9 = document.querySelector("main > section:first-of-type img:nth-of-type(9)");
+    pika9.classList.add("show");
+}
+
+button2017.addEventListener("click", showPika9);
+
+
+////
+//button en image 10
+////
+var button2018letsgo = document.querySelector("ol li:nth-of-type(10) button");
+
+function showPika10() {
+    var currentPika = document.querySelector("main > section:first-of-type img.show");
+    if(currentPika) {
+        currentPika.classList.remove("show")
+    }
+
+    var pika10 = document.querySelector("main > section:first-of-type img:nth-of-type(10)");
+    pika10.classList.add("show");
+}
+
+button2018letsgo.addEventListener("click", showPika10);
+
+
+////
+//button en image 11
+////
+var button2018smash = document.querySelector("ol li:nth-of-type(11) button");
+
+function showPika11() {
+    var currentPika = document.querySelector("main > section:first-of-type img.show");
+    if(currentPika) {
+        currentPika.classList.remove("show")
+    }
+
+    var pika11 = document.querySelector("main > section:first-of-type img:nth-of-type(11)");
+    pika11.classList.add("show");
+}
+
+button2018smash.addEventListener("click", showPika11);
+
+
+////
+//button en image 12
+////
+var button2019game = document.querySelector("ol li:nth-of-type(12) button");
+
+function showPika12() {
+    var currentPika = document.querySelector("main > section:first-of-type img.show");
+    if(currentPika) {
+        currentPika.classList.remove("show")
+    }
+
+    var pika12 = document.querySelector("main > section:first-of-type img:nth-of-type(12)");
+    pika12.classList.add("show");
+}
+
+button2019game.addEventListener("click", showPika12);
+
+
+////
+//button en image 13
+////
+var button2019movie = document.querySelector("ol li:nth-of-type(13) button");
+
+function showPika13() {
+    var currentPika = document.querySelector("main > section:first-of-type img.show");
+    if(currentPika) {
+        currentPika.classList.remove("show")
+    }
+
+    var pika13 = document.querySelector("main > section:first-of-type img:nth-of-type(13)");
+    pika13.classList.add("show");
+}
+
+button2019movie.addEventListener("click", showPika13);
+
+
+////
+//button en image 14
+////
+var button2022 = document.querySelector("ol li:nth-of-type(14) button");
+
+function showPika14() {
+    var currentPika = document.querySelector("main > section:first-of-type img.show");
+    if(currentPika) {
+        currentPika.classList.remove("show")
+    }
+
+    var pika14 = document.querySelector("main > section:first-of-type img:nth-of-type(14)");
+    pika14.classList.add("show");
+}
+
+button2022.addEventListener("click", showPika14);
+
+
+////
+//button en image 15
+////
+var button2023 = document.querySelector("ol li:nth-of-type(15) button");
+
+function showPika15() {
+    var currentPika = document.querySelector("main > section:first-of-type img.show");
+    if(currentPika) {
+        currentPika.classList.remove("show")
+    }
+
+    var pika15 = document.querySelector("main > section:first-of-type img:nth-of-type(15)");
+    pika15.classList.add("show");
+}
+
+button2023.addEventListener("click", showPika15);
+
+
+////
+//button en image 16
+////
+var buttonfight = document.querySelector("ol li:nth-of-type(16) button");
+
+function showPika16() {
+    var currentPika = document.querySelector("main > section:first-of-type img.show");
+    if(currentPika) {
+        currentPika.classList.remove("show")
+    }
+
+    var pika16 = document.querySelector("main > section:first-of-type img:nth-of-type(16)");
+    pika16.classList.add("show");
+}
+
+buttonfight.addEventListener("click", showPika16);
+
+
+////
+//button en image 17
+////
+var buttonsad = document.querySelector("ol li:nth-of-type(17) button");
+
+function showPika17() {
+    var currentPika = document.querySelector("main > section:first-of-type img.show");
+    if(currentPika) {
+        currentPika.classList.remove("show")
+    }
+
+    var pika17 = document.querySelector("main > section:first-of-type img:nth-of-type(17)");
+    pika17.classList.add("show");
+}
+
+buttonsad.addEventListener("click", showPika17);
+
+
+////
+//button en image 18
+////
+var buttonangry = document.querySelector("ol li:nth-of-type(18) button");
+
+function showPika18() {
+    var currentPika = document.querySelector("main > section:first-of-type img.show");
+    if(currentPika) {
+        currentPika.classList.remove("show")
+    }
+
+    var pika18 = document.querySelector("main > section:first-of-type img:nth-of-type(18)");
+    pika18.classList.add("show");
+}
+
+buttonangry.addEventListener("click", showPika18);
+
+
+////
+//button en image 19
+////
+var buttontail = document.querySelector("ol li:nth-of-type(19) button");
+
+function showPika19() {
+    var currentPika = document.querySelector("main > section:first-of-type img.show");
+    if(currentPika) {
+        currentPika.classList.remove("show")
+    }
+
+    var pika19 = document.querySelector("main > section:first-of-type img:nth-of-type(19)");
+    pika19.classList.add("show");
+}
+
+buttontail.addEventListener("click", showPika19);
+
+
+////
+//button en image 20
+////
+var buttonfunny = document.querySelector("ol li:nth-of-type(20) button");
+
+function showPika20() {
+    var currentPika = document.querySelector("main > section:first-of-type img.show");
+    if(currentPika) {
+        currentPika.classList.remove("show")
+    }
+
+    var pika20 = document.querySelector("main > section:first-of-type img:nth-of-type(20)");
+    pika20.classList.add("show");
+}
+
+buttonfunny.addEventListener("click", showPika20);
+
+
+////
+//button en image 21
+////
+var buttonketchup = document.querySelector("ol li:nth-of-type(21) button");
+
+function showPika21() {
+    var currentPika = document.querySelector("main > section:first-of-type img.show");
+    if(currentPika) {
+        currentPika.classList.remove("show")
+    }
+
+    var pika21 = document.querySelector("main > section:first-of-type img:nth-of-type(21)");
+    pika21.classList.add("show");
+}
+
+buttonketchup.addEventListener("click", showPika21);
+
+
+////
+//button en image 22
+////
+var buttonthunder = document.querySelector("ol li:nth-of-type(22) button");
+
+function showPika22() {
+    var currentPika = document.querySelector("main > section:first-of-type img.show");
+    if(currentPika) {
+        currentPika.classList.remove("show")
+    }
+
+    var pika22 = document.querySelector("main > section:first-of-type img:nth-of-type(22)");
+    pika22.classList.add("show");
+}
+
+buttonthunder.addEventListener("click", showPika22);
+
+
+////
+//button en image 23
+////
+var buttonattack = document.querySelector("ol li:nth-of-type(23) button");
+
+function showPika23() {
+    var currentPika = document.querySelector("main > section:first-of-type img.show");
+    if(currentPika) {
+        currentPika.classList.remove("show")
+    }
+
+    var pika23 = document.querySelector("main > section:first-of-type img:nth-of-type(23)");
+    pika23.classList.add("show");
+}
+
+buttonattack.addEventListener("click", showPika23);
+
+
+////
+//button en image 24
+////
+var buttondance = document.querySelector("ol li:nth-of-type(24) button");
+
+function showPika24() {
+    var currentPika = document.querySelector("main > section:first-of-type img.show");
+    if(currentPika) {
+        currentPika.classList.remove("show")
+    }
+
+    var pika24 = document.querySelector("main > section:first-of-type img:nth-of-type(24)");
+    pika24.classList.add("show");
+}
+
+buttondance.addEventListener("click", showPika24);
+
+
+//Sound effect
+//const audio = new Audio(“https://www.101soundboards.com/sounds/14748-22”);
+
+//const buttons = document.querySelectorAll(“button”);
+
+//buttons.forEach(button => {
+    //button.addEventListener(“click”, () => {
+      //audio.play();
+    //});
+  //});
